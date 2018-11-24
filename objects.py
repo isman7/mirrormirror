5cba87f94c31237a2437d71c0788659cd23c9fb0
Ismael Benito <isman7@gmail.com>
2018-11-24 11:40:29 +0100

    Features and objects updates

--- objects.py
+++ objects.py
@@ -2,6 +2,44 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+# Let's find againg the buttons from the panel with template matching:
+panel = cv2.imread("images/PC.png", cv2.IMREAD_GRAYSCALE)
+button = cv2.imread("images/Button.png", cv2.IMREAD_GRAYSCALE)
+w, h = button.shape
+
+result = cv2.matchTemplate(panel, button, cv2.TM_CCOEFF_NORMED)
+
+threshold = 0.8
+loc = np.where( result >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(panel, pt, (pt[0] + w, pt[1] + h), 255, 2)
+
+plt.figure()
+plt.imshow(panel, interpolation='none', cmap='gray')
+plt.show()
+
+# Let's use S channel:
+panel = cv2.imread("images/PC.png")
+button = cv2.imread("images/Button.png")
+w, h, *_ = button.shape
+
+panel_S = cv2.cvtColor(panel, cv2.COLOR_BGR2HSV)[:, :, 1]
+button_S = cv2.cvtColor(button, cv2.COLOR_BGR2HSV)[:, :, 1]
+
+result = cv2.matchTemplate(panel_S, button_S, cv2.TM_CCOEFF_NORMED)
+
+panel = cv2.cvtColor(panel, cv2.COLOR_BGR2GRAY)
+
+threshold = 0.8
+loc = np.where( result >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(panel, pt, (pt[0] + w, pt[1] + h), 255, 2)
+
+plt.figure()
+plt.imshow(panel, interpolation='none', cmap='gray')
+plt.show()
+
+
 # Let's import faces and eyes classifiers from OpenCV:
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
